30874e142e64f0979995c9ec799bddee7945a066
usha-12 <usha12ahirwar@gmail.com>
2022-10-27 19:18:53 +0530

    Create a Contact

--- src/com/bridgelabz/addressbooksystem/Contact.java
+++ src/com/bridgelabz/addressbooksystem/Contact.java
@@ -1,4 +1,47 @@
 package com.bridgelabz.addressbooksystem;
 
 public class Contact {
+   private String firstName;
+   private String lastName;
+   private String phoneNumber;
+   private String emailID;
+    public Contact(String firstName, String lastName,String phoneNumber, String emailID) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailID = emailID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
 }
